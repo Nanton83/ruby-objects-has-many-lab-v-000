7ce5915084502b851195035d690960eca623e156
Nick Romans <nicholas.romans@gmail.com>
2018-09-17 00:25:31 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,12 +2,12 @@ class Artist
   
   attr_accessor :name 
   
-  song_count = 0
+  @@song_count = 0
   
   def initialize(name)
     @name = name 
     @songs = []
-    
+    @@song_count += 1
   end 
   
   def songs
