ede453f615791231b8a07e8e84410cc82feacf25
Nick Romans <nicholas.romans@gmail.com>
2018-09-17 00:44:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,30 +1,4 @@
 
-# class Song
-  
-#   attr_accessor :artist, :name
-  
-#   @@all = []
-  
-#   def initialize(song, artist = nil)
-#     @@all << self
-#     @name = name
-#     @artist = artist 
-#   end 
-  
-#   def self.all
-#     @@all
-#   end 
-
-#   def artist_name
-#     if self.artist
-#       self.artist.name
-#     else
-#       nil 
-#     end 
-#   end 
-  
-# end 
-
 
 class Song
   
