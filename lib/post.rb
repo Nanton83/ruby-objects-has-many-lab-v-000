9c8683ac7daa8e2c51399244c8456a306de4b770
Nick Romans <nicholas.romans@gmail.com>
2018-09-17 00:54:40 +0000

    automatically backed up by learn

---
+++ lib/post.rb
@@ -0,0 +1,26 @@
+
+
+class Post
+  
+  attr_accessor :author, :name
+  
+  @@all = []
+  
+   def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end 
+  
+   def author_name
+    if author
+      self.author.name
+    else
+      nil
+    end
+  end
+  
+end
